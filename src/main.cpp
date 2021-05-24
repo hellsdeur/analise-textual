9181ea7723e585e09f7a3ce3f83b24ce158eb073
Helder Matos <mtshelder@gmail.com>
2021-05-24 02:04:24 -0300

    Aplicando regex nos textos e extraindo palavras no main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,38 @@
 #include "catalogo.h"
+#include "lista_encadeada/lista.h"
 #include <fstream>
+#include <string>
+#include <regex>
+#include <iostream>
 
 int main() {
 
-	// GerenciadorArquivos gerarq("../base_textos/");
-
 	std::ifstream arquivos[30];
+	ListaEncadeada lista;
 	
 	Catalogo cat("../base_textos/");
 
 	for (int i = 0; i < 30; i++) {
+		std::string s;
+
 		arquivos[i].open(cat.get_nome(i));
+
+		while (std::getline(arquivos[i], s)) {
+			std::regex r(R"([^\W_]+(?:['_-][^\W_]+)*)");
+			for(
+				std::sregex_iterator i = std::sregex_iterator(s.begin(), s.end(), r);
+				i != std::sregex_iterator();
+				++i) {
+    			std::smatch m = *i;
+				lista.inserir(m.str());
+			}
+			// std::regex_match(s, m, r);
+			// for (unsigned i = 0; i < m.size(); i++) {
+			// 	std::cout << m[i] << '\n';
+			// 	lista.inserir(m[i]);
+			// }
+			
+		}
 	}
 
 }

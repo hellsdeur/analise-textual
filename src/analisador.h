9b3d33e781ac24cb7da393c11c1bb49fccc802de
Helder Matos <mtshelder@gmail.com>
2021-06-04 08:37:15 -0300

    Corrigindo o preenchimento da lista de stop words

--- src/analisador.h
+++ src/analisador.h
@@ -6,7 +6,6 @@
 #include <utility>
 #include <iostream>
 #include <list>
-#include<algorithm>
 
 class Analisador {
 	private:
@@ -14,13 +13,12 @@ class Analisador {
 		std::unordered_map<std::string, int> dicionario;
 		std::list<std::pair<std::string, int>> lista;
 		std::list<std::string> stop_words;
-		std::string arr[200] = {"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"};
 
 	public:
 		Analisador(Catalogo c) {
 			this->c = c;
-			analisar();
 			preencher_stop_words();
+			analisar();
 		}
 
 		void preencher_stop_words() {
